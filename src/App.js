3c7bd79880ad29d8a1113c9b1b36d0614cbd3ebe
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-01 19:35:01 +0200

    display photos from api

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import SearchForm from './component/search_form';
 
 function App() {
 
+  const [id, setId] = useState(null);
   const [photos, setPhotos] = useState([]);
 
-  const fetchPhotos = (id) => {
-    console.log(id);
-    // const data = fetch(`https://jsonplaceholder.typicode.com/albums/${id}/photos`);
+  const fetchPhotos = async(id) => {
+    setId(id);
+    const data = await fetch(`https://jsonplaceholder.typicode.com/albums/${id}/photos`);
+    const result = await data.json();
+    console.log(result);
+    setPhotos(result);
   }
 
   return (
     <React.Fragment>
       <SearchForm fetchPhotos={fetchPhotos}/>
       <div className="content">
-
+       {photos.length > 0 && photos.map(photo => {
+         return <div key={photo.id}>
+           <h3>{photo.title}</h3>
+           <img src={photo.thumbnailUrl}/>
+         </div>
+       })
+       }
+       {
+         id && photos.length == 0 && <div>No photos in this album</div>
+       }
+       {
+         !id && photos.length == 0 && <div>Enter id to get photos</div>
+       }
       </div>
     </React.Fragment>
 
