d4812f16fd3b583932be4164165567c3953c5682
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-01 19:04:50 +0200

    add state to the search_form component

--- src/component/search_form.js
+++ src/component/search_form.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchForm = () => {
+
+    const [id, setId] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(id)
+    }
+
+    const handleChange = (e) => {
+        setId(e.target.value);
+    };
+
     return(
-        <form>
-            <input type="text"/>
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange} name="id" value={id}/>
             <button>Get Album Photos By Id</button>
         </form>
     )
