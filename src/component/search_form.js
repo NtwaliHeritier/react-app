497e090d7112137a98aa8b4fd988409170ae0465
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-01 18:57:16 +0200

    add search form

---
+++ src/component/search_form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchForm = () => {
+    return(
+        <form>
+            <input type="text"/>
+            <button>Get Album Photos By Id</button>
+        </form>
+    )
+}
+
+export default SearchForm;
